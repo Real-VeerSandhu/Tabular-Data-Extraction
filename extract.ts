cac98b453ddd900b65d0a337263c8860284f48ce
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-11 10:22:54 -0400

    add data map

--- extract.ts
+++ extract.ts
@@ -9,14 +9,15 @@ const data = result['Sheet1']
 
 for (let val of data) {
   if (typeof val['A'] == 'number') {
-    val['A'] = val['A'].toString()
+    val['A'] = val['A'].toString();
   }
 }
 
 
 console.log('----------') // 10558156
 
-const tabData = data.slice(3)
+// A: Part Number, B: Empty, C: Manufacturer, D: Description
 
+const tabData = data.slice(3);
 
-console.log(tabData);
+console.log(tabData[0]['C']); 
