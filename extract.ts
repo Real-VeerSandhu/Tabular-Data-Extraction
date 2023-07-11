763ea6ac37928187894f087cb5f47d3fafd9b241
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-11 11:28:07 -0400

    add ig gen

--- extract.ts
+++ extract.ts
@@ -13,11 +13,67 @@ for (let val of data) {
   }
 }
 
+function idGen(): string {
+    var S4 = function() {
+       return (((1+Math.random())*0x100)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
 
+console.log(data[1])
 console.log('----------') // 10558156
 
 // A: Part Number, B: Empty, C: Manufacturer, D: Description
 
 const tabData = data.slice(3);
 
-console.log(tabData[0]['C']); 
+// const testData: formatData = {
+//     partNumber: '12',
+//     manufacturer: 'me',
+//     description: 'yes'
+// }
+
+// console.log(testData)
+
+
+interface Item {
+    partNumber: string;
+    manufacturer: string;
+    description: string;
+}
+
+const formatData: Item[] = [];
+
+for (let i in tabData) {
+    const item: Item = {
+        partNumber: tabData[i]['A'],
+        manufacturer: tabData[i]['C'],
+        description: tabData[i]['D']
+    };
+    formatData.push(item)
+}
+
+const partNumberData: string[] = [];
+const manufacturerData: string[] = [];
+const descriptionData: string[] = [];
+const ids: string[] = [];
+
+for (let i in formatData) {
+    partNumberData.push(formatData[i]['partNumber'])
+    manufacturerData.push(formatData[i]['manufacturer'])
+    descriptionData.push(formatData[i]['description'])
+    ids.push(idGen())
+}
+
+function printData(): void {
+    console.log('IDs:', ids);
+    console.log('All Data:', formatData);
+    console.log('Part Numbers:', partNumberData);
+    console.log('Manufacturers:', manufacturerData);
+    console.log('Descriptions:', descriptionData);
+}
+
+// printData()
+
+
+
---
+++ test.ts
@@ -0,0 +1,10 @@
+function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x100)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+let x = guidGenerator()
+
+console.log(x)
