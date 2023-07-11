fa8085720f7977e8bb906132e2191dda189ef3d7
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-11 10:17:14 -0400

    add extraction

--- extract.ts
+++ extract.ts
@@ -5,4 +5,18 @@ const result = excelToJson({
     sourceFile: 'Data/LF14000NN.xlsx'
 });
 
-console.log(typeof result)
+const data = result['Sheet1']
+
+for (let val of data) {
+  if (typeof val['A'] == 'number') {
+    val['A'] = val['A'].toString()
+  }
+}
+
+
+console.log('----------') // 10558156
+
+const tabData = data.slice(3)
+
+
+console.log(tabData);
