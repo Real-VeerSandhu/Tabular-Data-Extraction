712a3cec12b0caba17921b08aa746dfe1cde9393
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-14 10:00:05 -0400

    add idGen

--- test.ts
+++ test.ts
@@ -1,5 +1,9 @@
 // Generate id
 
+/**
+ * Function to generate data ids
+ * @returns Unique random ID
+ */
 function guidGenerator() {
     var S4 = function() {
        return (((1+Math.random())*0x100)|0).toString(16).substring(1);
